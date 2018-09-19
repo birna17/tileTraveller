d9cf4d7512a462a2c80ad7a766fb2dbf812b7d86
Birna Valtýsdóttir <birnavaltys@gmail.com>
2018-09-19 16:11:22 +0000

    TileTraveller.py

---
+++ max_int.py
@@ -0,0 +1,6 @@
+num_int = int(input("Input a number: "))    # Do not change this line
+
+if num_int > 0:
+    print(input)
+elif num_int < 0:    
+    print("The maximum is", max_int)    # Do not change this line
